8d645b49120709e2f808293e1a4897061b7bf367
Serhii Fedusov <serhii.fedusov@depositphotos.com>
2020-10-05 22:52:17 +0300

    connected graphs and view, but improvements are needed

--- pacman.py
+++ pacman.py
@@ -1,6 +1,8 @@
 from turtle import *
 from freegames import floor, vector
 import random
+import time
+from graph import Graph
 
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
@@ -85,33 +87,28 @@ def world():
                 path.up()
                 path.goto(x + 10, y + 10)
 
-    pac_x, pac_y = random_init()
+    pac_x, pac_y, pac_raw = random_init()
     pacman = vector(pac_x, pac_y)
 
-    can_x, can_y = random_init()
-    while can_x == pac_x and can_y == pac_y:        # reinit in case of collision
-        can_x, can_y = random_init()
+    can_x, can_y, can_raw = random_init()
+    while can_x == pac_x and can_y == pac_y:  # reinit in case of collision
+        can_x, can_y, _ = random_init()
     path.up()
     path.goto(can_x + 10, can_y + 10)
     path.dot(5, 'white')
 
+    g = Graph(tiles)
+    way = g.DFS(pac_raw, can_raw)
+    return way
 
-def move():
+
+def move(way):
     "Move pacman"
     writer.undo()
 
     clear()
 
-    if valid(pacman + aim):
-        pacman.move(aim)
-
-    index = offset(pacman)
-
-    if tiles[index] == 1:
-        tiles[index] = 2
-        x = (index % 20) * 20 - 200
-        y = 180 - (index // 20) * 20
-        square(x, y)
+    pacman.move(way)
 
     up()
     goto(pacman.x + 10, pacman.y + 10)
@@ -119,37 +116,33 @@ def move():
 
     update()
 
-    ontimer(move, 100)
-
-
-def change(x, y):
-    "Change pacman aim if valid."
-    if valid(pacman + vector(x, y)):
-        aim.x = x
-        aim.y = y
-
 
 def random_init():
-    candy_pos = random.randint(0, len(tiles) + 1)
-    if tiles[candy_pos] == 0:
-        while tiles[candy_pos] != 1:
-            candy_pos = (candy_pos + 1) % len(tiles)
+    raw_pos = random.randint(0, len(tiles))
+    if tiles[raw_pos] == 0:
+        while tiles[raw_pos] != 1:
+            raw_pos = (raw_pos + 1) % len(tiles)
 
     # complex high-level mathematics
-    x = (candy_pos % 20) * 20 - 200
-    y = 180 - (candy_pos // 20) * 20
-    return x, y
+    x = (raw_pos % 20) * 20 - 200
+    y = 180 - (raw_pos // 20) * 20
+    return x, y, raw_pos
 
 
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 writer.goto(160, 160)
-listen()
-onkey(lambda: change(5, 0), 'Right')
-onkey(lambda: change(-5, 0), 'Left')
-onkey(lambda: change(0, 5), 'Up')
-onkey(lambda: change(0, -5), 'Down')
-world()
-move()
-done()
+way = world()
+prev = way[0]
+for next in way[1:]:
+    if next == prev + 1:
+        move(vector(20, 0))
+    elif next == prev - 1:
+        move(vector(-20, 0))
+    elif next < prev:
+        move(vector(0, 20))
+    elif next > prev:
+        move(vector(0, -20))
+    prev = next
+    time.sleep(0.3)
